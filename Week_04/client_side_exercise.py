d8187ea260666f077bbd8f24868c8e72d8697133
CallmeOnit <99156196+PNYWT@users.noreply.github.com>
2024-06-29 14:09:12 +0700

    update

---
+++ Week_04/client_side_exercise.py
@@ -0,0 +1,110 @@
+import sys
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from glfw.GLFW import *
+import numpy as np
+import pandas as pd
+import time, math as m
+
+win_w, win_h = 1024, 768
+
+def resize(window, w, h):
+    global win_w, win_h
+
+    win_w, win_h = w, h
+    glViewport(0, 0, w, h)  
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45, win_w/win_h, 0.01, 50)
+
+wireframe_on, animation_on = False, False
+def keyboard(window, key, scancode, action, mods):
+    global wireframe_on, animation_on
+
+    if action == GLFW_PRESS or action == GLFW_REPEAT:
+        if key == GLFW_KEY_SPACE:
+            animation_on = not animation_on
+        elif key == GLFW_KEY_W:
+            wireframe_on = not wireframe_on
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE if wireframe_on else GL_FILL)
+        elif key in (GLFW_KEY_ESCAPE, GLFW_KEY_Q):
+            glfwSetWindowShouldClose(window, GLFW_TRUE)
+    glfwPostEmptyEvent()
+
+ticks, frame_cnt = 0, 0
+def animation(window):
+    global ticks, frame_cnt
+
+    ticks += 1
+    frame_cnt += 1
+    glfwPostEmptyEvent()
+
+def refresh(window):
+    global start_time, frame_cnt
+    if frame_cnt == 20:
+        print("%.2f fps" % (frame_cnt/(time.time()-start_time)), ticks, end='\r')
+        start_time = time.time()
+        frame_cnt = 0    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    eye_pos = centroid + (0, 0, 1.5*max(bbox))
+    gluLookAt(*eye_pos, *centroid, 0, 1, 0)
+    light_pos = eye_pos
+
+    glRotatef(ticks, 0, 1, 0)
+
+    Kd = np.array((0, 1, 0), dtype=np.float32)
+    colors[:, :] = Kd
+
+    glfwSwapBuffers(window)
+
+def gl_init_models():
+    global start_time
+    global n_vertices, positions, colors, normals, uvs, centroid, bbox
+
+    glClearColor(0, 0, 0, 0)
+    glEnable(GL_DEPTH_TEST)
+    glShadeModel(GL_SMOOTH)
+
+    df = pd.read_csv("../models/bunny_uv.tri", sep='\s+',
+                     comment='#', header=None, dtype=np.float32)
+    centroid = df.values[:, 0:3].mean(axis=0)
+    bbox = df.values[:, 0:3].max(axis=0) - df.values[:, 0:3].min(axis=0)
+
+    n_vertices = len(df.values)
+    positions = np.ones((n_vertices, 3), np.float32)
+    normals = np.zeros((n_vertices, 3), np.float32)
+    positions[:, 0:3] = df.values[:, 0:3]
+    colors = df.values[:, 3:6]
+    normals[:, 0:3] = df.values[:, 6:9]
+    uvs = df.values[:, 9:11]
+    start_time = time.time() - 0.0001
+
+def main():
+    global window
+
+    if not glfwInit():
+        glfwTerminate()
+        return
+    
+    window = glfwCreateWindow(1, 1, "Client-side Vertex Arrays Exercise", None, None)
+    glfwMakeContextCurrent(window)  
+    glfwSetWindowRefreshCallback(window, refresh)
+    glfwSetWindowSizeCallback(window, resize)
+    glfwSetKeyCallback(window, keyboard)
+    glfwSetWindowPos(window, 20, 50)
+    glfwSetWindowSize(window, win_w, win_h)
+
+    gl_init_models()
+    while not glfwWindowShouldClose(window):
+        if animation_on:
+            animation(window)
+        refresh(window)
+        glfwWaitEvents()
+    glfwDestroyWindow(window)
+    glfwTerminate()
+
+if __name__ == "__main__":
+    main()
